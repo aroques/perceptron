9957f7992f92ce39a9b4d9bd5a8b5d65c218c05c
Alec Roques <alec.roques@gmail.com>
2018-05-03 15:51:31 -0500

    Refactor main

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 from random import choice
 from numpy import array, dot, random
 
-# TBD: make unit step the sign fn
-unit_step = lambda x: 0 if x < 0 else 1
 
-# Change training data to match sign function
-training_data = [
-    (array([0, 0, 1]), 0),
-    (array([0, 1, 1]), 1),
-    (array([1, 0, 1]), 1),
-    (array([1, 1, 1]), 1),
-]
+def main():
+    training_data = [
+        (array([0, 0, 1]), -1),
+        (array([0, 1, 1]), 1),
+        (array([1, 0, 1]), 1),
+        (array([1, 1, 1]), 1),
+    ]
 
-w = random.rand(3)
+    w = random.rand(3)
+    errors = []
+    eta = 0.2
+    n = 100
 
-errors = []
+    for i in range(n):
+        x, expected = choice(training_data)
+        result = dot(w, x)
+        error = expected - sign(result)
+        errors.append(error)
+        w += eta * error * x
 
-eta = 0.2
+    for x, _ in training_data:
+        result = dot(x, w)
+        print("{}: {} -> {}".format(x[:2], result, sign(result)))
 
-n = 100
 
-for i in range(n):
-    x, expected = choice(training_data)
-    result = dot(w, x)
-    error = expected - unit_step(result)
-    errors.append(error)
-    w += eta * error * x
+def sign(x):
+    return -1 if x < 0 else 1
 
-for x, _ in training_data:
-    result = dot(x, w)
-    print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
+
+if __name__ == '__main__':
+    main()
